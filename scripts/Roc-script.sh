3b5606abc159900a826637d05bfa9424dedc63e8
jeojeo1225 <47908936+jeojeo1225@users.noreply.github.com>
2026-03-25 02:01:00 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -24,24 +24,21 @@ fi
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x02000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
 sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x04000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x06000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
-
 # 调节IPQ60XX的1.5GHz频率电压(从0.9375V提高到0.95V，过低可能导致不稳定，过高可能增加功耗和发热，具体数值需要根据实际情况调整)
 # sed -i 's/opp-microvolt = <937500>;/opp-microvolt = <950000>;/' target/linux/qualcommax/patches-6.12/0038-v6.16-arm64-dts-qcom-ipq6018-add-1.5GHz-CPU-Frequency.patch
-
 # 1. 移除旧包（确保清理干净）
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-homeproxy
 rm -rf feeds/packages/net/sing-box
 rm -rf feeds/packages/lang/golang
-
 # 2. 克隆 Argon 主题与配置 (必须加回这两行)
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
-
-# 3. 克隆 HomeProxy 源码
+# 3. 克隆 HomeProxy与SINGBOX 源码,建议也补上最新的 golang 环境
 git clone --depth=1 https://github.com/immortalwrt/homeproxy package/homeproxy
-
+git clone --depth=1 https://github.com/sagernet/openwrt-sing-box package/sing-box
+git clone --depth=1 https://github.com/sbwml/packages_lang_golang package/golang
 # 4. 雅典娜LED控制
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
